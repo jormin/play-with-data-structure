01bf3df6d0245465a1176fe70f2ea2db6c6c609c
Jormin <jorminxee@gmail.com>
2021-02-20 20:34:34 +0800

    :sparkles: feat(1.0.0): 简单加法和高斯算法
    Signed-off-by: Jormin <jorminxee@gmail.com>

--- chapter2/gauss_sum.go
+++ chapter2/gauss_sum.go
@@ -1,6 +1,6 @@
 package chapter2
 
-// 高斯算法
+// 高斯算法，时间复杂度为O(1)
 func gaussSum(max int) (sum int) {
 	return (max + 1) * max / 2
 }
--- chapter2/simple_sum.go
+++ chapter2/simple_sum.go
@@ -1,6 +1,6 @@
 package chapter2
 
-// 简单加法
+// 简单加法，时间复杂度为O(n)
 func simpleSum(max int) (sum int) {
 	for i := 1; i <= max; i++ {
 		sum += i
